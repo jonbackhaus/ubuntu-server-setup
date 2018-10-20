67b3a649002a30200b73260a222bba74ca5cd54d
Jonathan Backhaus <jonathan.david.backhaus@gmail.com>
2018-10-20 13:52:50 -0400

    Removed password disable for SSH login (Will do separately post-config)

--- setupLibrary.sh
+++ setupLibrary.sh
@@ -47,7 +47,7 @@ function execAsUser() {
 # Modify the sshd_config file
 # shellcheck disable=2116
 function changeSSHConfig() {
-    sudo sed -re 's/^(\#?)(PasswordAuthentication)([[:space:]]+)yes/\2\3no/' -i."$(echo 'old')" /etc/ssh/sshd_config
+    # sudo sed -re 's/^(\#?)(PasswordAuthentication)([[:space:]]+)yes/\2\3no/' -i."$(echo 'old')" /etc/ssh/sshd_config
     sudo sed -re 's/^(\#?)(PermitRootLogin)([[:space:]]+)(.*)/PermitRootLogin no/' -i /etc/ssh/sshd_config
 }
 
